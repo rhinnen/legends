846bb162058390c3afcd36a877fefc5df3460211
Rory Hinnen <rorymh@me.com>
2019-07-20 11:04:40 -0500

    Initial Commit

---
+++ Legends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Legends
+//
+//  Created by Rory Hinnen on 7/20/19.
+//  Copyright © 2019 Rory Hinnen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
