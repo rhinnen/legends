859e7ed5077d49c526f2fa1a7c82b96fdc5d9cf8
Rory Hinnen <rorymh@me.com>
2019-07-20 19:14:18 -0500

    Start adding View Elements

--- Legends/Controller/ViewController.swift
+++ Legends/Controller/ViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ActionLabel: UILabel!
+    @IBOutlet weak var CharacterLabel: UILabel!
+    @IBOutlet var CharacterButton: UITapGestureRecognizer!
+    @IBOutlet var Stepper: [UIStepper]!
+    
+    var warband = Warband()
     override func viewDidLoad() {
         super.viewDidLoad()
+        if warband.characters.count == 0 {
+            CharacterLabel.text = "Add Characters"
+        }
+        
         // Do any additional setup after loading the view.
     }
 
